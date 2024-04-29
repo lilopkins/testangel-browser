009463fb079e3ac8c3e54537ede01f280063f4aa
Lily Hopkins <lily@hpkns.uk>
2024-04-29 12:45:29 +0100

    fix: removed unneeded mut

--- src/utils.rs
+++ src/utils.rs
@@ -7,7 +7,7 @@ pub fn serialise_elem(elem: WebElement) -> WebDriverResult<String> {
 }
 
 pub fn deserialise_elem<S: AsRef<str>>(handle: Arc<SessionHandle>, s: S) -> Result<WebElement, String> {
-    let mut s = s.as_ref();
+    let s = s.as_ref();
     let json_elem = serde_json::from_str(s).map_err(|e| format!("Invalid element parameter: {e}"))?;
     WebElement::from_json(json_elem, handle.clone()).map_err(|e| format!("Invalid element: {e}"))
 }
